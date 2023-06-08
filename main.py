2ecd0c9c4cb114da8e9c9e676933d384919ab726
Sam.E <samsmoke007@gmail.com>
2023-06-08 20:48:15 +0330

    use url_for for addresing css files in  htmls files

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 #start to make an app
 app = Flask(__name__)
 #making database
-file_dir = os.path.dirname(__file__)
+file_dir = os.path.dirname(os.path.abspath(__file__))
 goal_route = os.path.join(file_dir,"app.db")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + goal_route
 
@@ -28,7 +28,6 @@ class Shopping_Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    product = db.relationship('Product', backref=db.backref('carts', lazy=True))
 
     def making_dict(self):
         return {
